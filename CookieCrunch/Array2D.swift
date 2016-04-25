734c849e889043e6aa3fa28fc6d9741a609f60f5
Anthony Lewis <me@anthonylewis.com>
2016-04-24 19:45:41 -0500

    Initial Commit

---
+++ CookieCrunch/Array2D.swift
@@ -0,0 +1,30 @@
+//
+//  Array2D.swift
+//  CookieCrunch
+//
+//  Created by Anthony Lewis on 4/24/16.
+//  Copyright © 2016 Anthony Lewis. All rights reserved.
+//
+
+import Foundation
+
+struct Array2D<T> {
+    let columns: Int
+    let rows: Int
+    private var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        array = Array<T?>(count: rows*columns, repeatedValue: nil)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[row*columns + column]
+        }
+        set {
+            array[row*columns + column] = newValue
+        }
+    }
+}
--- CookieCrunch/GameViewController.swift
+++ CookieCrunch/GameViewController.swift
@@ -10,44 +10,47 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        if let scene = GameScene(fileNamed:"GameScene") {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
+    var scene: GameScene!
+    var level: Level!
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
-
+    
     override func shouldAutorotate() -> Bool {
         return true
     }
-
+    
     override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
-        } else {
-            return .All
-        }
+        return [.Portrait, .PortraitUpsideDown]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Configure the view.
+        let skView = view as! SKView
+        skView.multipleTouchEnabled = false
+        
+        // Create and configure the scene.
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        level = Level()
+        scene.level = level
+        
+        // Present the scene.
+        skView.presentScene(scene)
+        
+        beginGame()
     }
-
-    override func prefersStatusBarHidden() -> Bool {
-        return true
+    
+    func beginGame() {
+        shuffle()
+    }
+    
+    func shuffle() {
+        let newCookies = level.shuffle()
+        scene.addSpritesForCookies(newCookies)
     }
 }
